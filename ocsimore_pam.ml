7f2e2cec3cc3cd24d3aa638d016afec25a3862f5
balat at pps.jussieu.fr <none>
2008-06-22 22:36:51 +0200

    adding missing file ocsimore_pam.ml
    darcs-hash:20080622203651-1b86b-5fc95e018070b61bdae69b285f5b245ca07163ff

---
+++ ocsimore_pam.ml
@@ -0,0 +1,34 @@
+(* Ocsimore
+ * Copyright (C) 2008
+ * Laboratoire PPS - Université Paris Diderot - CNRS
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *)
+(**
+   @author Vincent Balat
+*)
+
+let pam_auth ?(service = "") ~name ~pwd () =
+  Lwt_preemptive.detach
+    (fun () ->
+       try
+         let pam = Pam.pam_start service ~user:name (fun _ _ -> pwd) in
+         Pam.pam_authenticate pam [] ~silent:true;
+         ignore (Pam.pam_end pam)
+       with Pam.Pam_Error _ -> raise Users.BadPassword
+    )
+    ()
+
+let _ = Session_manager.set_pam_auth pam_auth
