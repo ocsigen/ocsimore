7f1229f86e34300aae78fa9c8ba4f87ee73fffec
boris <boris@yakobowski.org>
2008-11-25 15:14:41 +0100

    Update on cascade in userrights
    darcs-hash:20081125141441-5c728-79e7dfced9905e5af861aaf024a63fa3b70cfe7a

--- modifs.sql
+++ modifs.sql
@@ -86,6 +86,13 @@ ALTER TABLE wikiboxwriters ADD FOREIGN KEY (writer) REFERENCES users
       ON DELETE CASCADE ON UPDATE CASCADE;
 
 
+ALTER TABLE userrights DROP CONSTRAINT userrights_groupid_fkey;
+ALTER TABLE userrights DROP CONSTRAINT userrights_id_fkey;
+ALTER TABLE userrights ADD FOREIGN KEY (groupid) REFERENCES users
+      ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE userrights ADD FOREIGN KEY (id) REFERENCES users
+      ON DELETE CASCADE ON UPDATE CASCADE;
+
 -- Content-type for wikiboxes
 ALTER TABLE wikiboxes ADD COLUMN content_type text NOT NULL DEFAULT 'wiki';
 
