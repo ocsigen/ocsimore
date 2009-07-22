ba942d0b043437de6f6bef7608f475a0a725cd8e
boris <boris@yakobowski.org>
2009-07-22 13:14:47 +0200

    File that was no longer compiling
    Ignore-this: 1547afec111aa81152283e1dc78cf9a1
    
    darcs-hash:20090722111447-5c728-ed5727bfd8366caf2b03d2c38777e0df0e5653b3

--- announce/eliom_atom.ml
+++ announce/eliom_atom.ml
@@ -3,15 +3,19 @@ let get_etag c = Some (Digest.to_hex (Digest.string c))
 
 open Ocsigen_http_frame
 
-let result_of_content (url, id, el) =
+let result_of_content (url, id, el) headers =
   let c = Atom_feed.simple_feed url id el in
   let md5 = get_etag c in
-  let default_result = default_result () in
-    {default_result with
+  let dr = default_result () in
+    {dr with
      res_content_length = Some (Int64.of_int (String.length c));
      res_content_type = Some "application/atom+xml";
      res_etag = md5;
-     res_headers= Http_headers.dyn_headers;
+     res_headers= (match headers with
+                         | None -> dr.res_headers
+                         | Some headers ->
+                             Http_headers.with_defaults headers dr.res_headers
+                      );
      res_stream =
         (Ocsigen_stream.make
             (fun () ->
@@ -22,8 +26,8 @@ let result_of_content (url, id, el) =
 module Reg_ = struct
   type page = (string * Atom_feed.identity * Atom_feed.entry list)
   type options
-  let send ?options ?(cookies=[]) ?charset ?code ~sp content =
-    Lwt.return (Eliom_services.EliomResult (result_of_content content))
+  let send ?options:_ ?(cookies=[]) ?charset:_ ?code:_ ?content_type:_ ?headers ~sp:_ content =
+    Lwt.return (result_of_content content headers)
 end
 
 include Eliom_mkreg.MakeRegister (Reg_)
