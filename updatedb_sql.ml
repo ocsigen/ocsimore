10f10af6e162e57d9e2b0fb32e7905638aab8ed0
boris <boris@yakobowski.org>
2009-11-12 17:11:36 +0100

    Missing file
    Ignore-this: 6d5f7efccfd35c63caef2c1c8afcdbc4
    
    darcs-hash:20091112161136-5c728-95ca807cef8e57cdbae2b33cdbf380cacfc9e2d3

---
+++ updatedb_sql.ml
@@ -0,0 +1,59 @@
+(* Ocsimore
+ * Copyright (C) 2005 Piero Furiesi Jaap Boender Vincent Balat
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *)
+
+open Sql
+let (>>=) = Lwt.bind
+
+
+let current_version = Lwt_unix.run
+  (Lwt.catch
+     (fun () ->
+        full_transaction_block
+          (fun db ->
+             PGSQL(db) "SELECT value FROM options WHERE name = 'dbversion'")
+        >>= fun l -> Lwt.return (int_of_string (List.hd l)))
+     (fun _ ->
+        Lwt.fail (Failure "Incorrect database version for ocsimore. Correct the key 'dbversion' in the table 'options'");
+     )
+  )
+
+let update_version db version =
+  let ver = string_of_int version in
+  PGSQL(db) "UPDATE options SET value = $ver WHERE name = 'dbversion'"
+
+let update version f =
+  if current_version < version then
+    full_transaction_block
+      (fun db ->
+         Printf.eprintf "Updating Ocsimore database to version %d\n%!" version;
+         f db >>= fun () ->
+         update_version db version)
+  else
+    Lwt.return ()
+
+
+let () = Lwt_unix.run
+ begin
+
+   update 2
+     (fun db -> PGSQL(db) "ALTER TABLE options ADD PRIMARY KEY (name)")
+   >>= fun () ->
+
+   Lwt.return ()
+
+ end
