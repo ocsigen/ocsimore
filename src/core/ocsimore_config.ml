88f68b74b14faf3ea82506c69e68a91dbab1f6bf
chambart <chambart@crans.org>
2011-07-20 18:21:19 +0200

    pguser/pgdatabase/pgpassword configuration parameters set the default runtime parameters
    Ignore-this: a3b661fc929d399a3935528d2192ac62
    
    darcs-hash:20110720162119-0cb73-8e8c9528b17df7202e3fe18ce27134e7c990238a

--- src/core/ocsimore_config.ml
+++
@@ -1,6 +0,0 @@
-let db_user = ref "ocsimore"
-let db_name = ref "ocsimore"
-
-let password = ref (try Sys.getenv "PGPASSWORD" with Not_found -> "")
-
-let dyngroupstobecreated = ref ([] : (string * Simplexmlparser.xml) list)
