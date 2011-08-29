8d4fe0f7b8ccc4836716976cbdeb380ee1685d91
gregoire.henry <gregoire.henry@pps.jussieu.fr>
2011-08-29 15:48:50 +0200

    ocamlbuild: more dependencies.
    Ignore-this: e543d9c6ccbe928ebe534407a9b7a04c
    
    darcs-hash:20110829134850-da4f2-3cc025d65e2d6f9833920e3fe0f68134bb357e64

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -588,8 +588,10 @@ let () =
 	 | Before_options ->
 	   tag_eliom_files ()
          | After_rules ->
-	   Pathname.define_context "src/site/server" ["src/wiki"];
-	   Pathname.define_context "src/site/client" ["src/wiki"];
+	   Pathname.define_context "src/site/server" ["src/wiki";"src/user";"src/core"];
+	   Pathname.define_context "src/site/client" ["src/wiki";"src/user";"src/core"];
+	   Pathname.define_context "src/forum" ["src/wiki";"src/user"];
+	   Pathname.define_context "src/user" ["src/core"];
 	   (* only works in subdirectories: no source at toplevel *)
 	   copy_rule "shared/*.ml -> client/*.ml"
 	     "%(path)/shared/%(file).ml" "%(path)/client/%(file).ml";
