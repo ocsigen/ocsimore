74340b87499a75d1cbf122f3a874f94f1a9baf54
zol <zol@benozol.de>
2012-04-10 11:37:45 +0200

    Restructure and document (!) configuration options
    Ignore-this: 34a3ffb77031256da25170a4762a11b5
    
    darcs-hash:20120410093745-4e2d2-edc1a78f88289c6a1e9b5862526e56430857b1d5

--- src/core/server/parse_config.ml
+++ src/core/server/parse_config.ml
@@ -23,33 +23,63 @@ Parsing the global Ocsimore configuration
 @author Vincent Balat
 *)
 
+let unexpected_pcdata ~in_tag pcdata =
+  Ocsigen_extensions.Error_in_config_file
+    ("Unexpected pcdata ..."^pcdata^"... inside "^in_tag)
+
+let unexpected_tag ~in_tag tag =
+  Ocsigen_extensions.Error_in_config_file
+    ("Unexpected tag \""^tag^"\" in content of \""^in_tag^"\"")
+
+let unexpected_content ~in_tag =
+  Ocsigen_extensions.Error_in_config_file
+    ("Unexpected content in tag \""^in_tag^"\"")
+
+let unexpected_value ~in_tag ~in_attribute value =
+  Ocsigen_extensions.Error_in_config_file
+    ("Unexpected value \""^value^"\" for attribute \""^in_attribute^"\" in tag \""^in_tag^"\"")
+
+let unexpected_attribute ~in_tag attr =
+  Ocsigen_extensions.Error_in_config_file
+    ("Unexpected attribute \""^attr^"\" inside tag \""^in_tag^"\"")
+
 let rec parse_config = function
   | [] -> ()
-  | (Simplexmlparser.Element ("passwordfile", [("name", name)], []))::l ->
-      (let c = open_in name in
-       try
-         let s = input_line c in
-         Ocsimore_config.db_password := Some s;
-         close_in c
-       with e -> close_in c; raise e);
+  | (Simplexmlparser.Element ("internationalization" as in_tag, attrs, content)) :: l ->
+       List.iter
+         (function | "language", "francais" ->
+                Language.messages := Language.messages_french
+            | "language", "english" ->
+                Language.messages := Language.messages_english
+            | "language" as in_attribute, lang ->
+                raise (unexpected_value ~in_tag ~in_attribute lang)
+            | attr, _ ->
+                raise (unexpected_attribute ~in_tag attr))
+         attrs;
+       if content <> [] then
+         raise (unexpected_content ~in_tag);
+       parse_config l
+  | (Simplexmlparser.Element ("mailer" as in_tag, attrs, content)) :: l ->
+      List.iter
+        (function | "bin", mailer ->
+               Ocsimore_config.mailer := mailer;
+           | attr, _ ->
+               raise (unexpected_attribute ~in_tag attr))
+        attrs;
+      if content <> [] then
+        raise (unexpected_content ~in_tag:"mailer");
       parse_config l
-  | (Simplexmlparser.Element ("group", [("name", name)], [s]))::l ->
-      Ocsimore_config.dyngroupstobecreated :=
-        (name, s)::!Ocsimore_config.dyngroupstobecreated;
+  | (Simplexmlparser.Element ("administration" as in_tag, attrs, content))::l ->
+       List.iter
+         (function | "path", path ->
+                Ocsimore_config.admin_dir := path;
+            | attr, _ ->
+                raise (unexpected_attribute ~in_tag attr))
+         attrs;
+       if content <> [] then
+         raise (unexpected_content ~in_tag);
       parse_config l
-  | (Simplexmlparser.Element ("language", [("lang", "francais")], []))::l ->
-      Language.messages := Language.messages_french;
-      parse_config l
-  | (Simplexmlparser.Element ("language", [("lang", "english")], []))::l ->
-      Language.messages := Language.messages_english;
-      parse_config l
-  | (Simplexmlparser.Element ("admin-dir", ["dir", dir], _)) :: l ->
-      Ocsimore_config.admin_dir := dir;
-      parse_config l
-  | (Simplexmlparser.Element ("mailer", ["bin", mailer], _)) :: l ->
-      Ocsimore_config.mailer := mailer;
-      parse_config l
-  | (Simplexmlparser.Element ("internals", attrs, []))::l ->
+  | (Simplexmlparser.Element ("internals" as in_tag, attrs, content))::l ->
        List.iter
          (function
             | "application-name", name ->
@@ -64,36 +94,56 @@ let rec parse_config = function
                 in
                 Ocsimore_config.aggregate_css := value
             | attr, _ ->
-                raise (Ocsigen_extensions.Error_in_config_file
-                         ("Unexpected attribute inside ocsimore 'internals' config: "^attr)))
+                raise (unexpected_attribute ~in_tag attr))
          attrs;
+       List.iter
+         (function | Simplexmlparser.Element ("group", [("name", name)], [s]) ->
+                Ocsimore_config.dyngroupstobecreated :=
+                  (name, s)::!Ocsimore_config.dyngroupstobecreated;
+            | Simplexmlparser.Element ("group", _, _) ->
+                raise (Ocsigen_extensions.Error_in_config_file
+                         ("Unexpected content for tag \"group\""))
+            | Simplexmlparser.Element (name, _, _) ->
+                raise (Ocsigen_extensions.Error_in_config_file
+                         ("Unexpected content \""^name^"\" in Ocsimore's tag internals"))
+            | Simplexmlparser.PCData pcdata ->
+                raise (unexpected_pcdata ~in_tag pcdata))
+         content;
+       if content <> [] then
+         raise (Ocsigen_extensions.Error_in_config_file "Unexpected content in tag \"internals\"");
        parse_config l
-  | (Simplexmlparser.Element ("database", attribs, []))::l ->
-    List.iter (function
-                  | "name", name -> Ocsimore_config.db_name := name;
-                  | "user", user -> Ocsimore_config.db_user := user
-                  | "host", host -> Ocsimore_config.db_host := Ocsimore_config.opt host
-                  | "port", port ->
-                    begin try
-                      Ocsimore_config.db_port :=
-                        Eliom_pervasives.map_option int_of_string (Ocsimore_config.opt port)
-                      with _ ->
-                        raise (Ocsigen_extensions.Error_in_config_file
-                                 ("Incorrect attribute inside ocsimore database config: port ("^port^") isn't an integer."))
-                    end
-                  | "socket-dir", dir ->
-                      Ocsimore_config.db_unix_domain_socket_dir := Ocsimore_config.opt dir
-                  | attrib, _ ->
-                    raise (Ocsigen_extensions.Error_in_config_file
-                             ("Unexpected attribute inside ocsimore database config: "^attrib)))
+  | (Simplexmlparser.Element ("database" as in_tag, attribs, content))::l ->
+    List.iter
+      (function | "name", name -> Ocsimore_config.db_name := name;
+         | "user", user -> Ocsimore_config.db_user := user
+         | "host", host -> Ocsimore_config.db_host := Ocsimore_config.opt host
+         | "port", port ->
+           begin try
+             Ocsimore_config.db_port :=
+               Eliom_pervasives.map_option int_of_string (Ocsimore_config.opt port)
+             with _ ->
+               raise (Ocsigen_extensions.Error_in_config_file
+                        ("Incorrect attribute inside ocsimore database config: port ("^port^") isn't an integer."))
+           end
+         | "socket-dir", dir ->
+             Ocsimore_config.db_unix_domain_socket_dir := Ocsimore_config.opt dir
+         | "password-file", name ->
+             (let c = open_in name in
+              try
+                let s = input_line c in
+                Ocsimore_config.db_password := Some s;
+                close_in c
+              with e -> close_in c; raise e)
+         | attr, _ ->
+             raise (unexpected_attribute ~in_tag attr))
       attribs;
+    if content <> [] then
+      raise (unexpected_content ~in_tag);
     parse_config l
   | Simplexmlparser.Element (name, _, _) :: _ ->
-      raise (Ocsigen_extensions.Error_in_config_file
-               ("Unexpected content <"^name^"> inside ocsimore config"))
+      raise (unexpected_tag ~in_tag:"ocsimore module" name)
   | Simplexmlparser.PCData pcdata :: _ ->
-      raise (Ocsigen_extensions.Error_in_config_file
-               ("Unexpected pcdata ..."^pcdata^"... inside ocsimore config"))
+      raise (unexpected_pcdata ~in_tag:"ocsimore module" pcdata)
 
 
 let () =
