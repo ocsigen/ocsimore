7ea77c41c5273d7ca76be8b327eee4c13fe091b2
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2012-08-21 14:21:34 +0200

    Add a little comment
    Ignore-this: f7ed50394f539d092f86d4a5154c9988
    
    darcs-hash:20120821122134-a85e5-f0a1b2bd4fcbcaf00172df7d2718df14a4324c3a

--- src/core/server/ocsi_sql.mli
+++ src/core/server/ocsi_sql.mli
@@ -15,7 +15,7 @@ val transaction_block : db_t -> (unit -> 'a Lwt.t) -> 'a Lwt.t
 (** Same as [transaction_block] but takes a db connection in the pool. *)
 val full_transaction_block : (db_t -> 'a Lwt.t) -> 'a Lwt.t
 
-(** *)
+(** Functions that transform 'a option to 'b Sql.t option *)
 val map_option_int32 : int32 option ->
   < nul : 'a; t : Sql.int32_t > Sql.t option
 val map_option_string : string option ->
