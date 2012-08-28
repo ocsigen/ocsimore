2a4349a0dd8711da4cf7d015811f0a36524310b4
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2012-08-28 17:30:32 +0200

    Database have now a default configuration
    Ignore-this: b298b2cca8f46a5cb86d0f9596a2f472
    
    darcs-hash:20120828153032-a85e5-f95e11a3efd02bd76231ecd2bfb27eadd7c1e928

--- src/core/server/ocsimore_config.ml
+++ src/core/server/ocsimore_config.ml
@@ -1,8 +1,8 @@
 let opt s = if s = "" then None else Some s
 
-let db_user = ref ""
-let db_name = ref ""
-let db_host = ref (opt "")
+let db_user = ref "ocsimore"
+let db_name = ref "ocsimore"
+let db_host = ref (opt "localhost")
 let db_port = ref (Eliom_lib.Option.map int_of_string None)
 let db_unix_domain_socket_dir = ref (opt "")
 let db_password = ref (opt "")
