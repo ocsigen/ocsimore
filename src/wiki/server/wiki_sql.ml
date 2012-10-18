17906e0d5164ef4dad8a69d992bd784235be4291
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2012-10-18 15:10:43 +0200

    Put back Wiki_sql.get_wiki_info_by_pages (used in ocsigen.org)
    Ignore-this: e0dc96a8e577cd5140d4de9ebb07c6bf
    
    darcs-hash:20121018131043-a85e5-04719e3504704814e45b91eda676df80092ca40e

--- src/wiki/server/wiki_sql.ml
+++ src/wiki/server/wiki_sql.ml
@@ -463,6 +463,15 @@ let find_wiki_by_name_ ?db name =
          | None -> Lwt.fail Not_found
     )
 
+let find_wiki_by_pages_ ?db page =
+  wrap db
+    (fun db ->
+      Lwt_Query.view_opt db (<:view< w | w in $wikis$; w.pages = $string:page$ >>)
+      >>= function
+        | Some c -> Lwt.return (reencapsulate_wiki c)
+        | None -> Lwt.fail Not_found
+    )
+
 let iter_wikis ?db f =
   wrap db
     (fun db ->
@@ -656,9 +665,18 @@ let cache_wiki_name =
   in
   new CWN.cache find_wiki_by_name_ 8
 
+let cache_wiki_pages =
+  let module CWN = Ocsigen_cache.Make(struct
+                                type key = string
+                                type value = wiki_info
+                              end)
+  in
+  new CWN.cache find_wiki_by_pages_ 8
+
 
 let get_wiki_info_by_id ~id = cache_wiki_id#find id
 let get_wiki_info_by_name ~name = cache_wiki_name#find name
+let get_wiki_info_by_pages ~pages = cache_wiki_pages#find pages
 
 let update_wiki ?db ?container ?staticdir ?path ?descr ?boxrights ?model ?siteid wiki =
   cache_wiki_id#remove wiki;
@@ -673,6 +691,7 @@ let delete_wiki ?(delete=true) wiki =
   (** Clear the cache before deleting *)
   cache_wiki_id#remove wiki;
   cache_wiki_name#clear ();
+  cache_wiki_pages#clear ();
   delete_wiki_ ~delete id
 
 
