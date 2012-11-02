da8923b30cfc4012dc383126ea54401777717332
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2012-11-02 14:41:57 +0100

    Add the _link value in the client-side module
    Ignore-this: a31ce3e576f6f5fcf3379ddaf333eb93
    
    darcs-hash:20121102134157-a85e5-435e7b186c36278257c27fc1180dda4f24c56614

--- src/site/client/ocsimore.ml
+++ src/site/client/ocsimore.ml
@@ -6,4 +6,8 @@ let v = (module Wiki_widgets : Null)
 let v = (module Forum_widgets : Null)
 let v = (module User_widgets : Null)
 
+(** Just a value to keep this module in the .js file even if there
+    is no explicit calls *)
+let _link = ()
+
 let () = Firebug.console##log(Js.string "ocsimore loaded")
