a444135bc14b7882add030b6195ae222035526d4
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2012-12-17 18:39:46 +0100

    Removing the postgres language (doesn't seems to work)
    Ignore-this: 2c5fd47ca1755865475ab995ceaca454
    
    darcs-hash:20121217173946-a85e5-09cf13fc4164acb478ed782461d7e345be07c995

--- src/files/initdb.sh
+++ src/files/initdb.sh
@@ -2,6 +2,4 @@
 
 createuser -P -d -S ocsimore
 createdb -E UTF-8 -O ocsimore ocsimore
-# TODO: Remove this. VVV
-createlang -U ocsimore plpgsql ocsimore
 psql -U ocsimore -f $(dirname $0)/createdb.sql ocsimore
