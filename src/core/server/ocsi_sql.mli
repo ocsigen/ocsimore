90710f228dc9062c43d4621fe1728a48dca1e6cb
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2013-10-17 00:08:59 +0200

    Use macaque_lwt

--- src/core/server/ocsi_sql.mli
+++ src/core/server/ocsi_sql.mli
@@ -10,19 +10,18 @@ type db_t = Lwt_PGOCaml.pa_pg_data Lwt_PGOCaml.t
 val pool : db_t Lwt_pool.t
 
 (** Perform an atomic transaction (using BEGIN and COMMIT/ROLLBACK *)
-val transaction_block : db_t -> (unit -> 'a Lwt.t) -> 'a Lwt.t
-
 (** Same as [transaction_block] but takes a db connection in the pool. *)
 val full_transaction_block : (db_t -> 'a Lwt.t) -> 'a Lwt.t
 
-val view : ('a, 'b) Sql.view -> 'a list Lwt.t
+val view : ?log:out_channel -> ('a, 'b) Sql.view -> 'a list Lwt.t
 
-val view_one : ('a, 'b) Sql.view -> 'a Lwt.t
+val view_one : ?log:out_channel -> ('a, 'b) Sql.view -> 'a Lwt.t
 
-val view_opt : ('a, 'b) Sql.view -> 'a option Lwt.t
+val view_opt : ?log:out_channel -> ('a, 'b) Sql.view -> 'a option Lwt.t
 
-val query : unit Sql.query -> unit Lwt.t
+val query : ?log:out_channel -> unit Sql.query -> unit Lwt.t
 
 val value :
+  ?log:out_channel ->
   < nul : Sql.non_nullable; t : 'a #Sql.type_info; .. > Sql.t ->
   'a Lwt.t
