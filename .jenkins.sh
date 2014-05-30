6d1494bdf50ad7005e2b7162f336996f8464326c
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-05-30 17:53:36 +0200

    TMP: hack

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,5 +1,5 @@
 opam pin ocsimore .
-opam pin macaque 'https://github.com/ocsigen/macaque.git#master'
+EDITOR="sed -i s/0.6.1/dev/" opam pin macaque 'https://github.com/ocsigen/macaque.git#master'
 opam install --deps-only ocsimore
 opam install --verbose ocsimore
 opam remove --verbose ocsimore
