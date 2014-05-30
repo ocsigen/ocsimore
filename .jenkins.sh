3107ae840f39c6461b4afeab2601ea0476ec6f6c
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-05-30 16:41:14 +0200

    jenkins: use the dev version of macaque

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,4 +1,5 @@
 opam pin ocsimore .
+opam pin macaque 'https://github.com/ocsigen/macaque.git#master'
 opam install --deps-only ocsimore
 opam install --verbose ocsimore
 opam remove --verbose ocsimore
