a1cee2a28d09ba38a636f36cdc58c8285e595ebd
Hugo Heuzard <hugo.heuzard@gmail.com>
2014-06-10 17:30:09 +0200

    update jenkins.sh: do not commit

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,5 +1,6 @@
 opam pin ocsimore .
 EDITOR="sed -i s/0.6.1/dev/" opam pin macaque 'https://github.com/ocsigen/macaque.git#master'
+opam pin eliom https://github.com/ocsigen/eliom.git#serv_typ
 opam install --deps-only ocsimore
 opam install --verbose ocsimore
 opam remove --verbose ocsimore
