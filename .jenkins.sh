a595c9db60db55bb5d35191767bfe41801a0b129
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-07-13 03:08:50 +0200

    Update .jenkins.sh (bis)

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,6 +1,6 @@
 opam pin add --no-action ocsimore .
 opam pin add --no-action ocsigenserver 'https://github.com/ocsigen/ocsigenserver.git#master'
-opam pin add --no-action eliom 'https://github.com/ocsigen/macaque.git#master'
+opam pin add --no-action eliom 'https://github.com/ocsigen/eliom.git#master'
 EDITOR="sed -i s/0.6.1/dev/" opam pin add --no-action macaque 'https://github.com/ocsigen/macaque.git#master'
 opam install --deps-only ocsimore
 opam install --verbose ocsimore
