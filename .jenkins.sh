2929dcbe2736d86e2102c81404d303854f1564d9
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-07-14 18:21:20 +0200

    Jenkins: build_doc.

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,7 +1,18 @@
+
 opam pin add --no-action ocsimore .
 opam pin add --no-action ocsigenserver 'https://github.com/ocsigen/ocsigenserver.git#master'
 opam pin add --no-action eliom 'https://github.com/ocsigen/eliom.git#master'
 EDITOR="sed -i s/0.6.1/dev/" opam pin add --no-action macaque 'https://github.com/ocsigen/macaque.git#master'
 opam install --deps-only ocsimore
 opam install --verbose ocsimore
-opam remove --verbose ocsimore
+
+do_build_doc () {
+  rm -rf _build/api.wikidocdir
+  make wiki-api
+  cp -Rf _build/api.wikidocdir/*.wiki $(API_DIR)/
+  cp -Rf doc/manual-wiki/*.wiki $(MANUAL_SRC_DIR)/
+}
+
+do_remove () {
+  opam remove --verbose ocsimore
+}
