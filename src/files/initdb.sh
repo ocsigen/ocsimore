1065ac5d7e5afe1f5b5c88e3cb30ccc4c6812f9a
Leo Valais <leo.valais97@gmail.com>
2018-12-18 15:39:10 +0100

    wikidoc: remove non-doc files

--- src/files/initdb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-createuser -P -d -S ocsimore
-createdb -E UTF-8 -O ocsimore ocsimore
-psql -U ocsimore -f $(dirname $0)/createdb.sql ocsimore
--- src/forum/forum_client.p.ml
+++
@@ -1,7 +0,0 @@
-
-let switchshow =
-  fun.client (i1 : string) (i2 : string) -> 
-  Js.Node.set_attribute (Js.get_element_by_id i2) "style" "display:block";
-  Js.Node.set_attribute (Js.get_element_by_id i1) "style" "display:none"
-
-
